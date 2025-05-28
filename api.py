734518e331727d4ef3b1ca9398780400fc78dbb5
Kenny Lin <lin.kenneth2001@gmail.com>
2025-05-28 14:32:34 -0700

    Update api.py
    Added get_mount_name()

--- api.py
+++ api.py
@@ -99,6 +99,27 @@ def check_item_is_mount(BEARER_TOKEN, item_id):
 
 	return (data.get("item_class").get("id") == 15 and data.get("item_subclass").get("id") == 5)
 
+def get_mount_name(BEARER_TOKEN, item_id):
+	"""
+	Gets mount name from item id
+	"""
+	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/item/{item_id}?namespace=static-us&locale=en_US"
+	response = requests.get(
+		REQUEST_URL,
+		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
+	)
+	try:
+		print(f"Getting mount name from item id...")
+		response.raise_for_status()
+		data = response.json()
+	except requests.HTTPError as e:
+		print(f"HTTP Error: {e}")
+		raise e
+	except Exception as e:
+		print(f"Unexpected Exception: {e}")
+		raise e
+	return data.get("preview_item").get("spells")[0].get("spell").get("name")
+
 if __name__ == '__main__':
 	print(len(get_achieve_index(auth.get_access_token())))
 	
