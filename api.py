bd8f6143c1ad868358ab6a4c61339f0b635f1000
Kenny Lin <lin.kenneth2001@gmail.com>
2025-05-28 11:34:08 -0700

    Update api.py
    Get achievement reward implemented

--- api.py
+++ api.py
@@ -50,6 +50,33 @@ def get_achieve_index(BEARER_TOKEN):
 		raise e
 	return data["achievements"]
 
+def get_achieve_reward(BEARER_TOKEN, achieve_id):
+	"""
+	Fetch Achievement Reward
+	"""
+	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/achievement/{achieve_id}?namespace=static-us&locale=en_US"
+	response = requests.get(
+		REQUEST_URL,
+		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
+	)
+	try:
+		print("Requesting Achievement Reward...")
+		response.raise_for_status()
+		data = response.json()
+	except requests.HTTPError as e:
+		print(f"HTTP Error: {e}")
+		raise e
+	except Exception as e:
+		print(f"Unexpected Exception: {e}")
+		raise e
+	
+	if (data.get("reward_description") == None):
+		return None
+	elif (not data.get("reward_description").startswith("Mount: ")):
+		return None
+	else:
+		return data.get("reward_item").get("id")
+
 if __name__ == '__main__':
 	print(len(get_achieve_index(auth.get_access_token())))
 	
