25130cc0dd858abed15e34c09b85b738b91f0b14
Kenny Lin <lin.kenneth2001@gmail.com>
2025-05-28 10:42:20 -0700

    Update test_module.py
    Added achievement index

--- test_module.py
+++ test_module.py
@@ -9,9 +9,14 @@ class TestAPI(unittest.TestCase):
     #         auth.get_access_token()
     
     def test_mount_index(self):
-        self.assertTrue(type(api.get_mount_index(auth.get_access_token())) == dict)
+        self.assertTrue(type(api.get_mount_index(auth.get_access_token())) == list)
         with self.assertRaises(requests.HTTPError):
             api.get_mount_index("wrong")
+
+    def test_achieve_index(self):
+        self.assertTrue(type(api.get_achieve_index(auth.get_access_token())) == list)
+        with self.assertRaises(requests.HTTPError):
+            api.get_achieve_index("wrong")
 
 if __name__ == '__main__':
     unittest.main()
