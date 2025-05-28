83d6cb0126f488ff2d9472271915ce50bb1177c5
Kenny Lin <lin.kenneth2001@gmail.com>
2025-05-28 11:10:44 -0700

    Update test_module.py
    Store bearer token in class variable

--- test_module.py
+++ test_module.py
@@ -7,20 +7,21 @@ class TestAPI(unittest.TestCase):
     # def test_access_token(self):
     #     with self.assertRaises(requests.HTTPError):
     #         auth.get_access_token()
+    BEARER_TOKEN = auth.get_access_token()
     
     def test_mount_index(self):
-        self.assertTrue(type(api.get_mount_index(auth.get_access_token())) == list)
+        self.assertTrue(type(api.get_mount_index(self.BEARER_TOKEN)) == list)
         with self.assertRaises(requests.HTTPError):
             api.get_mount_index("wrong")
 
     def test_achieve_index(self):
-        self.assertTrue(type(api.get_achieve_index(auth.get_access_token())) == list)
+        self.assertTrue(type(api.get_achieve_index(self.BEARER_TOKEN)) == list)
         with self.assertRaises(requests.HTTPError):
             api.get_achieve_index("wrong")
 
     def test_achieve_reward(self):
-        self.assertTrue(api.get_achieve_reward(6) == None)
-        self.assertTrue(api.get_achieve_reward(4602) == 51954)
+        self.assertTrue(api.get_achieve_reward(self.BEARER_TOKEN, 6) == None)
+        self.assertTrue(api.get_achieve_reward(self.BEARER_TOKEN, 4602) == 51954)
 
 if __name__ == '__main__':
     unittest.main()
