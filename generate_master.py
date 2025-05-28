fc4cd794c1273a699026d3ec7ed56623bfc19528
Kenny Lin <lin.kenneth2001@gmail.com>
2025-05-28 01:34:57 -0700

    Update generate_master.py
    Removed json handling for keys

--- generate_master.py
+++ generate_master.py
@@ -1,19 +1,11 @@
+import os
 import json
 import requests
+import unittest
+from dotenv import load_dotenv
 
-def get_client(file_name : str) -> str:
-    try:
-        with open(file_name, 'r') as file:
-            data = json.load(file)
-            return data['id'], data['secret']
-    except FileNotFoundError:
-        print(f"Error: File not found at path: {file_name}")
-        return None
-    except json.JSONDecodeError:
-        print(f"Error: Invalid JSON format in file: {file_name}")
-        return None
-    except KeyError as e:
-        print(f"Error: Invalid key: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return None
+# Load environmental variables from .env
+load_dotenv()
+
+if __name__ == '__main__':
+  print(os.getenv("CLIENT_ID"))
