a519f160bd9efad90337d270bbd0eb1efcf004e6
Kenny Lin <lin.kenneth2001@gmail.com>
2025-05-28 02:04:40 -0700

    Rename generate_master.py to api.py
    Added mount_index request

---
+++ api.py
@@ -0,0 +1,30 @@
+import auth
+import requests
+import unittest
+from dotenv import load_dotenv
+
+# Load environmental variables from .env
+load_dotenv()
+
+def get_mount_index(BEARER_TOKEN):
+	REQUEST_URL = "https://us.api.blizzard.com/data/wow/mount/index?namespace=static-us&locale=en_US"
+	response = requests.get(
+		REQUEST_URL,
+		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
+	)
+	try:
+		print("Requesting Mount Index...")
+		response.raise_for_status()
+		data = response.json()
+		print("Success")
+	except requests.HTTPError as e:
+		print(f"HTTP Error: {e}")
+		raise e
+	except Exception as e:
+		print(f"Unexpected Exception: {e}")
+		raise e
+	return data["mounts"]
+
+if __name__ == '__main__':
+	print(len(get_mount_index(auth.get_access_token())))
+	
--- test_module.py
+++ test_module.py
@@ -1,11 +1,17 @@
 import unittest
 import auth
+import api
 import requests
 
 class TestAPI(unittest.TestCase):
-    def test_access_token(self):
+    # def test_access_token(self):
+    #     with self.assertRaises(requests.HTTPError):
+    #         auth.get_access_token()
+    
+    def test_mount_index(self):
+        self.assertTrue(type(api.get_mount_index(auth.get_access_token())) == dict)
         with self.assertRaises(requests.HTTPError):
-            auth.get_access_token()
+            api.get_mount_index("wrong")
 
 if __name__ == '__main__':
     unittest.main()
