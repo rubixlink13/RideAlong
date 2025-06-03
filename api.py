eb1aedcad94ba05f2ac1f2596a81232f25356f21
Kenny Lin <lin.kenneth2001@gmail.com>
2025-06-02 23:22:25 -0700

    Restructured api.py
    Functions now return raw json dict. Data handling will be done in a different file.

--- api.py
+++ api.py
@@ -5,252 +5,82 @@ from dotenv import load_dotenv
 # Load environmental variables from .env
 load_dotenv()
 
+def _api_request(REQUEST_URL, BEARER_TOKEN, msg):
+	response = requests.get(
+		REQUEST_URL,
+		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
+	)
+	try:
+		print(msg)
+		response.raise_for_status()
+		data = response.json()
+		print("Success")
+	except requests.HTTPError as e:
+		print(f"HTTP Error: {e}")
+		raise e
+	except Exception as e:
+		print(f"Unexpected Exception: {e}")
+		raise e
+	print(type(data))
+	return data
+
 
 def get_mount_index(BEARER_TOKEN):
 	"""
 	Fetch Mount Index
 	"""
 	REQUEST_URL = "https://us.api.blizzard.com/data/wow/mount/index?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print("Requesting Mount Index...")
-		response.raise_for_status()
-		data = response.json()
-		print("Success")
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
-	return data["mounts"]
+	
+	return _api_request(REQUEST_URL, BEARER_TOKEN, "Requesting Mount Index...")
 
 def get_achieve_index(BEARER_TOKEN):
 	"""
 	Fetch Achievement Index
 	"""
 	REQUEST_URL = "https://us.api.blizzard.com/data/wow/achievement/index?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print("Requesting Achievement Index...")
-		response.raise_for_status()
-		data = response.json()
-		print("Success")
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
-	return data["achievements"]
+	
+	return _api_request(REQUEST_URL, BEARER_TOKEN, "Requesting Achievement Index...")
 
 def get_encounter_index(BEARER_TOKEN):
 	"""
 	Fetch Encounter Index
 	"""
 	REQUEST_URL = "https://us.api.blizzard.com/data/wow/journal-encounter/index?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print("Requesting Encounter Index...")
-		response.raise_for_status()
-		data = response.json()
-		print("Success")
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
 	
-	print(len(data["encounters"]))
-	return data["encounters"]
+	return _api_request(REQUEST_URL, BEARER_TOKEN, "Requesting Encounter Index...")
 
-def get_achieve_reward(BEARER_TOKEN, achieve_id):
-	"""
-	Fetch Achievement Reward
-	"""
-	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/achievement/{achieve_id}?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print(f"Requesting Achievement Reward from {achieve_id}...")
-		response.raise_for_status()
-		data = response.json()
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
-	
-	if (data.get("reward_description") == None):
-		return None
-	elif (not data.get("reward_description").startswith("Mount: ")):
-		return None
-	else:
-		return data.get("reward_item").get("id")
-	
 def get_achieve_info(BEARER_TOKEN, achieve_id):
 	"""
-	Get dict of achievement name and category from achievement id
+	Gets achievement info (dict) from achievement id
 	"""
 	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/achievement/{achieve_id}?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print(f"Getting dict of Achievement info for Achievement {achieve_id}...")
-		response.raise_for_status()
-		data = response.json()
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
-	
-	return {"name" : data.get("name"), "category" : data.get("category").get("name")}
-	
-def check_item_is_mount(BEARER_TOKEN, item_id):
+
+	return _api_request(REQUEST_URL, BEARER_TOKEN, f"Requesting Achievement Info from {achieve_id}...")
+
+
+def get_item_info(BEARER_TOKEN, item_id):
 	"""
-	Checks if item is mount
+	Gets item info (dict) from item id
 	"""
 	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/item/{item_id}?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print(f"Checking if item {item_id} is mount...")
-		response.raise_for_status()
-		data = response.json()
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
 
-	return (data.get("item_class").get("id") == 15 and data.get("item_subclass").get("id") == 5)
+	return _api_request(REQUEST_URL, BEARER_TOKEN, f"Requesting Item Info from {item_id}...")
 
-def get_item_name(BEARER_TOKEN, item_id):
+def get_mount_search(BEARER_TOKEN, mount_name):
 	"""
-	Gets item name from item id
-	"""
-	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/item/{item_id}?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print(f"Getting item name from item {item_id}...")
-		response.raise_for_status()
-		data = response.json()
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
-	return data.get("name")
-
-def get_mount_name(BEARER_TOKEN, item_id):
-	"""
-	Gets mount name from item id
-	"""
-	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/item/{item_id}?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization" : f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print(f"Getting mount name from item {item_id}...")
-		response.raise_for_status()
-		data = response.json()
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
-	return data.get("preview_item").get("spells")[0].get("spell").get("name")
-
-def get_mount_id(BEARER_TOKEN, mount_name):
-	"""
-	Gets mount id from mount name
+	Gets mount search (dict) from mount name
 	"""
 	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/search/mount?namespace=static-us&name.en_US={mount_name.lower().replace(" ", "%20")}&orderby=id&_page=1"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization": f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print(f"Getting mount id from mount {mount_name}...")
-		response.raise_for_status()
-		data = response.json()
-	except requests.HTTPERROR as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
-	for i in data.get("results"):
-		if i.get("data").get("name").get("en_US") == mount_name:
-			return i.get("data").get("id")
-		
-def get_encounter_drops(BEARER_TOKEN, encounter_id):
-	"""
-	Gets list of encounter drops from encounter id
-	"""
-	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/journal-encounter/{encounter_id}?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization": f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print(f"Getting list of encounter drops from encounter {encounter_id}...")
-		response.raise_for_status()
-		data = response.json()
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
-	return data.get("items")
 
+	return _api_request(REQUEST_URL, BEARER_TOKEN, f"Requesting Mount Search for {mount_name}...")
+		
 def get_encounter_info(BEARER_TOKEN, encounter_id):
 	"""
-	Gets dict with instance name, encounter name, and instance type from encounter id
+	Gets encounter info (dict) from encounter id
 	"""
 	REQUEST_URL = f"https://us.api.blizzard.com/data/wow/journal-encounter/{encounter_id}?namespace=static-us&locale=en_US"
-	response = requests.get(
-		REQUEST_URL,
-		headers = {"Authorization": f"Bearer {BEARER_TOKEN}"}
-	)
-	try:
-		print(f"Getting dict of encounter info from encounter {encounter_id}...")
-		response.raise_for_status()
-		data = response.json()
-	except requests.HTTPError as e:
-		print(f"HTTP Error: {e}")
-		raise e
-	except Exception as e:
-		print(f"Unexpected Exception: {e}")
-		raise e
-	return {"type" : data.get("category").get("type"), "name" : data.get("name"), "instance" : data.get("instance").get("name")}
+
+	return _api_request(REQUEST_URL, BEARER_TOKEN, f"Requesting Encounter Info from {encounter_id}...")
 
 
 if __name__ == '__main__':
--- test_module.py
+++ test_module.py
@@ -10,51 +10,40 @@ class TestAPI(unittest.TestCase):
     BEARER_TOKEN = auth.get_access_token()
     
     def test_mount_index(self):
-        self.assertTrue(type(api.get_mount_index(self.BEARER_TOKEN)) == list)
+        self.assertTrue(type(api.get_mount_index(self.BEARER_TOKEN)) == dict)
         with self.assertRaises(requests.HTTPError):
             api.get_mount_index("wrong")
 
     def test_achieve_index(self):
-        self.assertTrue(type(api.get_achieve_index(self.BEARER_TOKEN)) == list)
+        self.assertTrue(type(api.get_achieve_index(self.BEARER_TOKEN)) == dict)
         with self.assertRaises(requests.HTTPError):
             api.get_achieve_index("wrong")
 
     def test_encounter_index(self):
-        self.assertTrue(type(api.get_encounter_index(self.BEARER_TOKEN)) == list)
+        self.assertTrue(type(api.get_encounter_index(self.BEARER_TOKEN)) == dict)
         with self.assertRaises(requests.HTTPError):
             api.get_encounter_index("wrong")
 
-    def test_achieve_reward(self):
-        self.assertTrue(api.get_achieve_reward(self.BEARER_TOKEN, 6) == None)
-        self.assertTrue(api.get_achieve_reward(self.BEARER_TOKEN, 4602) == 51954)
-        self.assertTrue(api.get_achieve_reward(self.BEARER_TOKEN, 19343) == None)
-
     def test_achieve_info(self):
-        self.assertTrue(api.get_achieve_info(self.BEARER_TOKEN, 4602) == {"name" : "Glory of the Icecrown Raider (10 player)", "category" : "Dungeons & Raids"})
+        self.assertTrue(type(api.get_achieve_info(self.BEARER_TOKEN, 6)) == dict)
+        with self.assertRaises(requests.HTTPError):
+            api.get_achieve_info("wrong", 6)
 
-    def test_check_item_is_mount(self):
-        self.assertFalse(api.check_item_is_mount(self.BEARER_TOKEN, 19019))
-        self.assertTrue(api.check_item_is_mount(self.BEARER_TOKEN, 51954))
+    def test_item_info(self):
+        self.assertTrue(type(api.get_item_info(self.BEARER_TOKEN, 51954)) == dict)
+        with self.assertRaises(requests.HTTPError):
+            api.get_item_info("wrong", 51954)
 
-    def test_item_name(self):
-        self.assertTrue(api.get_item_name(self.BEARER_TOKEN, 51954) == "Reins of the Bloodbathed Frostbrood Vanquisher")
-
-    def test_mount_name(self):
-        self.assertTrue(api.get_mount_name(self.BEARER_TOKEN, 51954) == "Bloodbathed Frostbrood Vanquisher")
-        self.assertTrue(api.get_mount_name(self.BEARER_TOKEN, 184183) == "Voracious Gorger")
-
-    def test_mount_id(self):
-        self.assertTrue(api.get_mount_id(self.BEARER_TOKEN, "Voracious Gorger") == 1443)
-        self.assertTrue(api.get_mount_id(self.BEARER_TOKEN, "Bloodbathed Frostbrood Vanquisher") == 365)
-
-    def test_encounter_drops(self):
-        self.assertTrue(type(api.get_encounter_drops(self.BEARER_TOKEN, 1636)) == list)
-        self.assertTrue(type(api.get_encounter_drops(self.BEARER_TOKEN, 2519)) == list)
+    def test_mount_search(self):
+        self.assertTrue(type(api.get_mount_search(self.BEARER_TOKEN, "Voracious Gorger")) == dict)
+        with self.assertRaises(requests.HTTPError):
+            api.get_mount_search("wrong", "Voracious Gorger")
 
     def test_encounter_info(self):
-        self.assertTrue(api.get_encounter_info(self.BEARER_TOKEN, 1636) == {"type": "RAID", "name": "The Lich King", "instance": "Icecrown Citadel"}) 
-        self.assertTrue(api.get_encounter_info(self.BEARER_TOKEN, 2519) == {"type": "RAID", "name": "Fyrakk the Blazing", "instance": "Amirdrassil, the Dream's Hope"}) 
-        self.assertTrue(api.get_encounter_info(self.BEARER_TOKEN, 2396) == {"type": "DUNGEON", "name": "Nalthor the Rimebinder", "instance": "The Necrotic Wake"})
+        self.assertTrue(type(api.get_encounter_info(self.BEARER_TOKEN, 1636)) == dict)
+        with self.assertRaises(requests.HTTPError):
+            api.get_encounter_info("wrong", 1636)
+
 
 if __name__ == '__main__':
     unittest.main()
