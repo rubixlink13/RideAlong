c0e156b7744c4afeb62f4ce8af20104683930166
Kenny Lin <lin.kenneth2001@gmail.com>
2025-06-04 12:05:57 -0700

    Update master.py
    Added generation of encounter mounts to module

--- master.py
+++ master.py
@@ -1,4 +1,5 @@
 import api
+import requests
 from mount import Mount, Source, Achievement, Drop
 
 def generate(BEARER_TOKEN):
@@ -28,16 +29,51 @@ def add_achievement_mounts(BEARER_TOKEN, master):
             item_id = achieve_info.get("reward_item").get("id")
             item_name = achieve_info.get("reward_item").get("name")
             item_info = api.get_item_info(BEARER_TOKEN, item_id)
-            mount_name = item_info.get("preview_item").get("name")
+            mount_name = item_info.get("preview_item").get("spells")[0].get("spell").get("name")
             mount_id = _get_mount_id(BEARER_TOKEN, mount_name)
             achieve_name = achieve_info.get("name")
-            category = achieve_info.get("category").get("name")
+            category = achieve_info.get("category")
 
             master.append(Mount(mount_id, mount_name, item_id, item_name, Achievement(category, achieve_id, achieve_name)))
 
-
 def add_encounter_mounts(BEARER_TOKEN, master):
-    pass
+    """
+    Adds encounter mounts to the master list using the following steps:
+    1. Create encounter index
+    2. For each encounter, get list of drops
+    3. For each drop, check if drop is mount
+    4. If mount, get item ID
+    5. Convert item ID to Mount object
+    6. Add Mount object to master list
+    """
+
+    index = api.get_encounter_index(BEARER_TOKEN)
+    encounter_list = index.get("encounters")
+
+    for encounter in encounter_list:
+        encounter_info = api.get_encounter_info(BEARER_TOKEN, encounter.get("id"))
+        drops_list = encounter_info.get("items")
+        for drop in drops_list:
+            try:
+                item_info = api.get_item_info(BEARER_TOKEN, drop.get("item").get("id"))
+            except requests.HTTPError as e:
+                print(f"Item {drop.get("item").get("id")} not found")
+                continue
+            if item_info.get("item_class").get("id") == 15 and item_info.get("item_subclass").get("id") == 5:
+                mount_name = item_info.get("preview_item").get("spells")[0].get("spell").get("name")
+                mount_id = _get_mount_id(BEARER_TOKEN, mount_name)
+                item_id = item_info.get("id")
+                item_name = item_info.get("name")
+                category = encounter_info.get("category")
+                instance_id = encounter_info.get("instance").get("id")
+                instance_name = encounter_info.get("instance").get("name")
+                encounter_id = encounter_info.get("id")
+                encounter_name = encounter_info.get("name")
+
+                master.append(Mount(mount_id, mount_name, item_id, item_name, Drop(category, instance_id, instance_name, encounter_id, encounter_name)))
+        
+
+
 
 
 def _get_mount_id(BEARER_TOKEN, mount_name):
@@ -55,4 +91,5 @@ if __name__ == "__main__":
     import auth
     BEARER_TOKEN = auth.get_access_token()
     master = generate(BEARER_TOKEN)
-    print(master)
+    for mount in master:
+        print(str(mount))
