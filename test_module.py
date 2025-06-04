84d570e10b09cc7f0c5bfeaa9829825a5d815475
Kenny Lin <lin.kenneth2001@gmail.com>
2025-06-03 20:28:40 -0700

    Update test_module.py
    Wrote unittests for Mount class (achievement source)

--- test_module.py
+++ test_module.py
@@ -1,6 +1,7 @@
 import unittest
 import auth
 import api
+from mount import Mount, Source, Achievement, Drop
 import requests
 
 class TestAPI(unittest.TestCase):
@@ -43,6 +44,33 @@ class TestAPI(unittest.TestCase):
         self.assertTrue(type(api.get_encounter_info(self.BEARER_TOKEN, 1636)) == dict)
         with self.assertRaises(requests.HTTPError):
             api.get_encounter_info("wrong", 1636)
+
+class TestAchieveMount(unittest.TestCase):
+    achieve_mount = Mount(364, "Icebound Frostbrood Vanquisher", 51955, "Reins of the Icebound Frostbrood Vanquisher", Achievement("Dungeons & Raids", 4603, "Glory of the Icecrown Raider (25 player)"))
+    
+    def test_mount_id(self):
+        self.assertTrue(self.achieve_mount.get_mount_id() == 364)
+
+    def test_mount_name(self):
+        self.assertTrue(self.achieve_mount.get_mount_name() == "Icebound Frostbrood Vanquisher")
+
+    def test_item_id(self):
+        self.assertTrue(self.achieve_mount.get_item_id() == 51955)
+    
+    def test_item_name(self):
+        self.assertTrue(self.achieve_mount.get_item_name() == "Reins of the Icebound Frostbrood Vanquisher")
+
+    def test_source(self):
+        self.assertTrue(type(self.achieve_mount.get_source()) == Achievement)
+
+    def test_category(self):
+        self.assertTrue(self.achieve_mount.get_source().get_category() == "Dungeons & Raids")
+
+    def test_achieve_id(self):
+        self.assertTrue(self.achieve_mount.get_source().get_achievement_id() == 4603)
+
+    def test_achieve_name(self):
+        self.assertTrue(self.achieve_mount.get_source().get_achievement_name() == "Glory of the Icecrown Raider (25 player)")
 
 
 if __name__ == '__main__':
