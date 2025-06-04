2315e00a14ba45b7bb2b499a80053d5eedd8cd5f
Kenny Lin <lin.kenneth2001@gmail.com>
2025-06-03 17:55:38 -0700

    Update mount.py
    Change Source, Achievement, and Drop to global scope

--- mount.py
+++ mount.py
@@ -27,49 +27,48 @@ class Mount():
     def get_source(self):
         return self.source
 
-    class Source():
-        category = ""
+class Source():
+    category = ""
 
-        def get_category(self):
-            return self.category
-        
-    class Achievement(Source):
-        achievement_id = -1
-        achievement_name = ""
-        
-        def __init__(self, category, achievement_id, achievement_name):
-            self.category = category
-            self.achievement_id = achievement_id
-            self.achievement_name = achievement_name
-        
-        def get_achievement_id(self):
-            return self.achievement_id
-        
-        def get_achievement_name(self):
-            return self.achievement_name
+    def get_category(self):
+        return self.category
     
-    class Drop(Source):
-        instance_id = -1
-        instance_name = ""
-        encounter_id = -1
-        encounter_name = ""
-
-        def __init__(self, category, instance_id, instance_name, encounter_id, encounter_name):
-            self.category = category
-            self.instance_id = instance_id
-            self.instance_name = instance_name
-            self.encounter_id = encounter_id
-            self.encounter_name = encounter_name
-
-        def get_instance_id(self):
-            return self.instance_id
-        
-        def get_instance_name(self):
-            return self.instance_name
-        
-        def get_encounter_id(self):
-            return self.encounter_id
-        
-        def get_encounter_name(self):
-            return self.encounter_name
+class Achievement(Source):
+    achievement_id = -1
+    achievement_name = ""
     
+    def __init__(self, category, achievement_id, achievement_name):
+        self.category = category
+        self.achievement_id = achievement_id
+        self.achievement_name = achievement_name
+    
+    def get_achievement_id(self):
+        return self.achievement_id
+    
+    def get_achievement_name(self):
+        return self.achievement_name
+
+class Drop(Source):
+    instance_id = -1
+    instance_name = ""
+    encounter_id = -1
+    encounter_name = ""
+
+    def __init__(self, category, instance_id, instance_name, encounter_id, encounter_name):
+        self.category = category
+        self.instance_id = instance_id
+        self.instance_name = instance_name
+        self.encounter_id = encounter_id
+        self.encounter_name = encounter_name
+
+    def get_instance_id(self):
+        return self.instance_id
+    
+    def get_instance_name(self):
+        return self.instance_name
+    
+    def get_encounter_id(self):
+        return self.encounter_id
+    
+    def get_encounter_name(self):
+        return self.encounter_name
